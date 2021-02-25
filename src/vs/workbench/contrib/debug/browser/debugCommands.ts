faa67497bf9ca5adbcdf99dcd3e1fd6311ba7841
qibo <qibo@zhejianglab.com>
2021-02-25 10:37:23 +0800

    fix path

--- src/vs/workbench/contrib/debug/browser/debugCommands.ts
+++ src/vs/workbench/contrib/debug/browser/debugCommands.ts
@@ -5,6 +5,7 @@
 
 import * as childprocess from 'child_process';
 import { INativeEnvironmentService } from 'vs/platform/environment/common/environment';
+import * as path from 'path';
 import * as nls from 'vs/nls';
 import { KeyCode, KeyMod } from 'vs/base/common/keyCodes';
 import { List } from 'vs/base/browser/ui/list/listWidget';
@@ -395,7 +396,6 @@ export function registerCommands(): void {
 			const debugService = accessor.get(IDebugService);
 			let { launch, name, getConfig } = debugService.getConfigurationManager().selectedConfiguration;
 			const config = await getConfig();
-			console.log("config: " + config);
 			const clonedConfig = deepClone(config);
 			await debugService.startDebugging(launch, clonedConfig || name, { noDebug: debugStartOptions && debugStartOptions.noDebug });
 		}
@@ -410,7 +410,7 @@ export function registerCommands(): void {
 		handler: async (accessor: ServicesAccessor) => {
 			const nativeEnvironmentService = accessor.get(INativeEnvironmentService);
 			const editorService = accessor.get(IEditorService);
-			childprocess.exec('node ' + nativeEnvironmentService.appRoot + '/extensions/parserdrawio/src/main.js ' + editorService?.activeEditor?.resource?.fsPath, (error, stdout, stderr) => {
+			childprocess.exec('node ' + path.join(nativeEnvironmentService.appRoot + '/extensions/parserdrawio/src/main.js ') + editorService?.activeEditor?.resource?.fsPath, (error, stdout, stderr) => {
 				if (error instanceof Error) {
 					console.error(error);
 					throw error;
